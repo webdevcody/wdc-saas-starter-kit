19dd5adec4e0276676994da25baefe9cbc198a53
jerebenitez <jeremias.benitez@mi.unc.edu.ar>
2024-08-14 15:56:37 -0300

    fix clash on conflict when re-sending a reset token
    Since the target was set to resetToken.id instead of resetToken.userId, if I clicked twice on "Send Reset Email" it would throw an "Dduplicate key values violates unique constraints" error instead of refreshing the token in the db.

--- src/data-access/reset-tokens.ts
+++ src/data-access/reset-tokens.ts
@@ -19,7 +19,7 @@ export async function createPasswordResetToken(userId: UserId) {
       tokenExpiresAt,
     })
     .onConflictDoUpdate({
-      target: resetTokens.id,
+      target: resetTokens.userId,
       set: {
         token,
         tokenExpiresAt,
